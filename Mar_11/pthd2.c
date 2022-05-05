4624220ddaa3100957927a32c6b561c00c3d332d
VanditPrajapati <98203765+VanditPrajapati@users.noreply.github.com>
2022-05-05 17:07:54 +0530

    Update pthd2.c

--- Mar_11/pthd2.c
+++ Mar_11/pthd2.c
@@ -14,7 +14,7 @@ int main(int argc,char* argv[])
 pthread_t t1;
 void *res;
 int s;
-s= pthread_create(&t1,NULL,threadfun,"Hello Devesh");
+s= pthread_create(&t1,NULL,threadfun,"Hello Vandit");
 printf("Main function \n");
 sleep(1);
 exit(0);
