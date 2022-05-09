9b64e548a0664ef7ab4a8f08c658ebaa96ff6c60
VanditPrajapati <98203765+VanditPrajapati@users.noreply.github.com>
2022-05-09 16:15:47 +0530

    Add files via upload

--- mentor_assignment/sched_task.c
+++ mentor_assignment/sched_task.c
@@ -9,29 +9,11 @@
 
 pthread_t tid;
 
-/*void *thread(void *arg)
+void *thread(void *arg)
 {
 	int l=0, u=9;
 	int r_value;
-	while(1)
-	{
-		// For generating random value
-		srand(time(0));
-		r_value = (rand()%(u-l+1))+l;
-		
-		//If random value is equal to the value obtain from file then terminate the thread
-		if(r_value==arg)
-		{
-			pthread_exit(NULL);
-		}
-	}
-	return NULL;
-}*/
-
-void fun(int v)
-{
-	int l=0, u=9;
-	int r_value;
+	int *v=(int *)arg;
 	while(1)
 	{
 		// For generating random value
@@ -41,10 +23,10 @@ void fun(int v)
 		//If random value is equal to the value obtain from file then terminate the thread
 		if(r_value==v)
 		{
-			return NULL;
+			printf("Done\n");
+			pthread_exit(NULL);
 		}
 	}
-	return NULL;	
 }
 
 int main(void)
@@ -53,22 +35,21 @@ int main(void)
 	char read_buf[1024];
 	FILE *fp;
 	
-	int p, q=2, r=0;
+	int p=0, q=2, r=0;
 	int t[20];
 	
 	int value=5;
 
 	char *str;
-	char *s1;
+	char s1[2];
 	
 	struct json_object *parsed_json;
 	struct json_object *day;
 	
 	size_t ndays;
+	
 
-	//Defining the time object	
-
-  	
+	//Reading from json file  	
   	fp=fopen("hours.json","r");
 	fread(read_buf, 1024, 1, fp);
 	fclose(fp);
@@ -86,41 +67,43 @@ int main(void)
 	}	
 	
 	printf("%s\n", str);
-	 
-	for(p=0; str[p]!='\0'; p=p+3)
+	
+	//Storing the time string into array and then converting it into integer  
+	for(p=0; str[p]!='\0'; p=p+1)
 	{
-		strncpy(s1, &str[p], q);
+		//memcpy(s1, &str[p], q);
+		for(int c=0;c<2;c++)
+		{
+			s1[c]=str[p];
+			p=p+1;
+		}
 		s1[2]='\0';
 		t[r]=atoi(s1);
 		r=r+1;
-		q=q+3;
+		//q=q+3;
 	}
+	
 	
 	for(int i=0;i<6;i++)
 	{
 		printf("%d\n", t[i]);
 	} 
+
 	  	
-  	//Running the code between 10am to 5pm
+  	//Running the code between defined time 
   	while(1)
 	{
 		time_t rawtime;
   		struct tm * timeinfo;
   		time( &rawtime );
 		timeinfo = localtime(&rawtime);
-		printf("%02d:%02d:%02d", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+		printf("%02d:%02d:%02d\t", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
 		
 		if((timeinfo->tm_hour >= t[0] & timeinfo->tm_min > t[1]) && (timeinfo->tm_hour <= t[3] & timeinfo->tm_min < t[4] ))
-		{	
-			//Reading the value from json file	
+		{		
+			pthread_create(&tid, NULL, thread, (void *)value);
 		
-			//pthread_create(&tid, NULL, thread, value);
-		
-			fun(value);
-		
-			//pthread_join(tid, NULL);
-		
-			printf("Done\n");	
+			pthread_join(tid, NULL);	
 		}
 		else
 		{
